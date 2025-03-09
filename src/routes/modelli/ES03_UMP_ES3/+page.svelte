d40c303867b99c41f6a6874455f60af4290beab6
AntikGhalt <paolorefuto@gmail.com>
2025-03-09 12:37:34 +0100

    Aggiunte cartelle /modelli/esercizi + pages

---
+++ src/routes/modelli/ES03_UMP_ES3/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Vediamo from "$lib/components/vediamo.svelte";
+    const defaultEmbedUrl = "https://www.desmos.com/3d/o8ee7ymaep";
+    
+    // test formule con displaystyle diverso, proabably una merda 
+  </script>
+  
+  <div>
+    <Vediamo
+      title="UMP ES. 3"
+      description="Esercizio Utility Maximization Problem basato su perfetti complementi:
+          "
+      formula= false
+      defaultEmbedUrl = {defaultEmbedUrl}
+      newTabUrl = {defaultEmbedUrl}
+      editUrl = {defaultEmbedUrl}
+    />
+  </div>
+  
+  <style>
+    /* .intro-box {
+        background-color: #fff;
+        border: 1px solid #ddd;
+        border-radius: 12px;
+        padding: 20px 30px;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+      } */
+  </style>
+  
---
+++ src/routes/modelli/ES06_EMP_CES/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Vediamo from "$lib/components/vediamo.svelte";
+    const defaultEmbedUrl = "https://www.desmos.com/3d/6a4xbspbnb";
+    
+    // test formule con displaystyle diverso, proabably una merda 
+  </script>
+  
+  <div>
+    <Vediamo
+      title="EMP Esercizio con CES"
+      description="Esercizio Expenditure Minimization Problem basato su funzione di utilità Constant Elasticity of Substitution:
+      "
+      formula= false
+      defaultEmbedUrl = {defaultEmbedUrl}
+      newTabUrl = {defaultEmbedUrl}
+      editUrl = {defaultEmbedUrl}
+    />
+  </div>
+  
+  <style>
+    /* .intro-box {
+        background-color: #fff;
+        border: 1px solid #ddd;
+        border-radius: 12px;
+        padding: 20px 30px;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+      } */
+  </style>
+  
